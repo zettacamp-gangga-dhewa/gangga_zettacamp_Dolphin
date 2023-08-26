60cfc14b329af7867f807130a4bd9b6ce83fcaa5
zettacamp-gangga-dhewa <gangga.dhewa@zettacamp.pro>
2023-08-26 23:30:09 +0700

    JS Day 1 Daily Task

---
+++ JS1_gangga.js
@@ -0,0 +1,34 @@
+const books=[1,2,3];
+
+let book1= "When Mockingbird Sing";
+const book2= "Cloud Atlas";
+// let book4;
+
+let book3="White Fang";
+
+
+books[1]=book1;
+books[2]=book2;
+books[3]=book3;
+
+books[2]=books[2];
+
+// 1a
+book1="Atomic Habits";
+
+// 1b
+// book2="Laskar Pelangi";
+
+// 2
+allbook=books[1].concat(books[2]);
+
+
+console.log("My Favourite Book :",books[2]);
+
+console.log("My Favourite Book :",books[2]);
+// console.log("My Favourite Book :",book4);
+
+
+// concat2
+console.log("\nBook Collection :\n",book1 +"\n"+ book2);
+//console.log("\nBook Collection :\n",allbook);
