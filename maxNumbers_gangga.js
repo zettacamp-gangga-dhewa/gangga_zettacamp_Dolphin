a8d7dc45fe3e26b42e536ca636b09b176a07275d
zettacamp-gangga-dhewa <gangga.dhewa@zettacamp.pro>
2023-08-26 23:17:45 +0700

    JS Day 2 Logic Test

---
+++ maxNumbers_gangga.js
@@ -0,0 +1,10 @@
+// Write a function max_of_two(a, b) that takes in two integers, a and b, and returns the maximum of the two numbers without using any arrays or built-in functions like max().
+
+
+function max_of_two(a, b) {
+    return a > b ? a : b;
+    // return a < b ? b : a;
+}
+
+console.log(max_of_two(10, 5));
+console.log(max_of_two(45, 66));
